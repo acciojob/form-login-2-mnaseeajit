35bb4dad88f476a4c497b8ab83890737391be0de
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-06 16:24:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 //your JS code here. If required.
-function showAlert(){
-	alert( "localHost 3000says");
+function showAlert() {
+  var firstName = document.getElementById("firstname").value;
+  var lastName = document.getElementById("lastname").value;
+  var phoneNumber = document.getElementById("phoneNumber").value;
+  var email = document.getElementById("email").value;
+
+  alert(
+    "localhost 3000 says\n" +
+    "First Name: " + firstName + "\n" +
+    "Last Name: " + lastName + "\n" +
+    "Phone Number: " + phoneNumber + "\n" +
+    "Email ID: " + email
+  );
 }
